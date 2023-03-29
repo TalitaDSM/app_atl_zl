50f0217a68b8275f3124db3233ba427d3c17a60c
Talita Alves <talita.santos46@fatec.sp.gov.br>
2023-03-29 15:16:19 -0300

    instanciando classe customColors

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -12,119 +12,147 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   Color topColor = const Color.fromARGB(255, 200, 200, 200);
   Color bottomColor = Color.fromARGB(255, 229, 229, 229);
+
+  bool continueConnected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                topColor,
-                bottomColor,
-              ],
-            ),
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              topColor,
+              bottomColor,
+            ],
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 7,
-                  ),
-                  child: Image.asset(
-                    "assets/logo_atletica.png",
-                    height: 125,
-                  ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 7,
                 ),
+                child: Image.asset(
+                  "assets/logo_atletica.png",
+                  height: 125,
+                ),
+              ),
+              // ignore: prefer_const_constructors
+              Text(
+                "Entrar",
+                textAlign: TextAlign.center,
                 // ignore: prefer_const_constructors
-                Text(
-                  "Entrar",
-                  textAlign: TextAlign.center,
-                  // ignore: prefer_const_constructors
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 99, 1, 1),
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+                style: TextStyle(
+                  color: Color.fromARGB(255, 99, 1, 1),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-                Form(
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        autofocus: true,
-                        style: TextStyle(color: Color.fromARGB(255, 99, 1, 1)),
-                        decoration: InputDecoration(
-                          labelText: "E-mail",
-                          labelStyle: TextStyle(
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      autofocus: true,
+                      style: TextStyle(color: Color.fromARGB(255, 99, 1, 1)),
+                      decoration: InputDecoration(
+                        labelText: "E-mail",
+                        labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 99, 1, 1),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.mail_outline,
+                          color: Color.fromARGB(255, 99, 1, 1),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
                             color: Color.fromARGB(255, 99, 1, 1),
                           ),
-                          prefixIcon: Icon(
-                            Icons.mail_outline,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
                             color: Color.fromARGB(255, 99, 1, 1),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 99, 1, 1),
-                            ),
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 99, 1, 1),
-                            ),
                           ),
                         ),
                       ),
-                      TextFormField(
-                        style: TextStyle(color: Color.fromARGB(255, 99, 1, 1)),
-                        decoration: InputDecoration(
-                          labelText: "Senha",
-                          labelStyle: TextStyle(
+                    ),
+                    TextFormField(
+                      style: TextStyle(color: Color.fromARGB(255, 99, 1, 1)),
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 99, 1, 1),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.vpn_key_sharp,
+                          color: Color.fromARGB(255, 99, 1, 1),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
                             color: Color.fromARGB(255, 99, 1, 1),
-                          ),
-                          prefixIcon: Icon(
-                            Icons.vpn_key_sharp,
-                            color: Color.fromARGB(255, 99, 1, 1),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 99, 1, 1),
-                            ),
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 99, 1, 1),
-                            ),
                           ),
                         ),
-                      )
-                    ],
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(255, 99, 1, 1),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print("FUNCIONOU!");
+                },
+                child: Text(
+                  "Esqueceu a senha?",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 99, 1, 1),
+                    fontSize: 12,
                   ),
+                  textAlign: TextAlign.right,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    print("FUNCIONOU!");
-                  },
-                  child: Text(
-                    "Esqueceu a senha?",
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Row(
+                children: [
+                  Checkbox(
+                    value: continueConnected,
+                    onChanged: (value) {
+                      setState(() {
+                        continueConnected = value!;
+                        print(continueConnected);
+                      });
+                    },
+                  ),
+                  Text(
+                    "Continuar conectado?",
                     style: TextStyle(
                       color: Color.fromARGB(255, 99, 1, 1),
-                      fontSize: 12,
                     ),
-                    textAlign: TextAlign.right,
-                  ),
-                ),
-              ],
-            ),
-          )),
+                  )
+                ],
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("Login"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/values/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  Color activePrimaryButton = Color.fromARGB(255, 63, 81, 181);
+
+  Color getActiveprimaryButtonColor() {
+    return activePrimaryButton;
+  }
+}
